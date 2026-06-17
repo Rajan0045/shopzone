6ce9a396aa6e9e933b71e7307d9a8a45017516e4
Rajan <chaudharyrajan721@gmail.com>
2026-06-17 21:15:32 +0530

    changes

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -7,18 +7,15 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
     const persistedState = localStorage.getItem("persist:root");
-
     if (persistedState) {
         const rootState = JSON.parse(persistedState);
         const userState = JSON.parse(rootState.user);
-
-        const token = userState?.user?.token;
-
+        const token = userState?.user?.user?.token;
+        console.log("token:", userState);
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
     }
-
     return config;
 });
 
